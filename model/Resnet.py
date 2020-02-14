ea230d4284d165d2ec83e2a00240ed535202afed
Niels Ole Salscheider <salscheider@fzi.de>
2020-02-14 14:58:38 +0100

    New resnet layout

--- model/Resnet.py
+++ model/Resnet.py
@@ -33,30 +33,35 @@ class ResnetModule(tf.keras.Model):
 
         self.bn1 = OnlineNorm()
         self.bn2 = OnlineNorm()
+        self.bn3 = OnlineNorm()
 
-        self.conv1 = tf.keras.layers.SeparableConv2D(
-            self.num_filters[0],
+        self.conv1 = tf.keras.layers.Conv2D(self.num_filters[0],
+            (1, 1),
+            use_bias=False,
+            kernel_initializer=tf.keras.initializers.he_normal(),
+            kernel_regularizer=tf.keras.regularizers.l2(L2_REGULARIZER_WEIGHT),
+            name='conv1')
+
+        self.conv2 = tf.keras.layers.SeparableConv2D(self.num_filters[1],
             (3, 3),
+            use_bias=False,
             padding='same',
             strides=(self.stride, self.stride),
             dilation_rate=(self.dilation_rate, self.dilation_rate),
             kernel_initializer=tf.keras.initializers.he_normal(),
             kernel_regularizer=tf.keras.regularizers.l2(L2_REGULARIZER_WEIGHT),
-            name='conv1')
+            name='conv2')
 
-        self.conv2 = tf.keras.layers.SeparableConv2D(
-            self.num_filters[1],
-            (3, 3),
-            padding='same',
-            dilation_rate=(self.dilation_rate, self.dilation_rate),
+        self.conv3 = tf.keras.layers.Conv2D(self.num_filters[2],
+            (1, 1),
             kernel_initializer=tf.keras.initializers.he_normal(),
             kernel_regularizer=tf.keras.regularizers.l2(L2_REGULARIZER_WEIGHT),
-            name='conv2')
+            name='conv3')
 
         if self.has_skip_conv:
             self.bn_skip = OnlineNorm()
 
-            self.conv_skip = tf.keras.layers.Conv2D(self.num_filters[1],
+            self.conv_skip = tf.keras.layers.Conv2D(self.num_filters[-1],
                 (1, 1),
                 strides=(self.stride, self.stride),
                 kernel_initializer=tf.keras.initializers.he_normal(),
@@ -73,6 +78,9 @@ class ResnetModule(tf.keras.Model):
         x = self.bn2(x, training=train_batch_norm)
         x = tf.keras.activations.relu(x)
         x = self.conv2(x)
+        x = self.bn3(x, training=train_batch_norm)
+        x = tf.keras.activations.relu(x)
+        x = self.conv3(x)
         x = x + skip
         return x
 
@@ -88,18 +96,16 @@ class ResnetBackbone(tf.keras.Model):
             kernel_regularizer=tf.keras.regularizers.l2(L2_REGULARIZER_WEIGHT),
             name='first_conv')
 
-        self.bn1 = OnlineNorm()
-        self.maxpool1 = tf.keras.layers.MaxPooling2D(2)
-        self.maxpool2 = tf.keras.layers.MaxPooling2D(2)
+        self.padding1 = tf.keras.layers.ZeroPadding2D((1, 1))
+        self.maxpool1 = tf.keras.layers.MaxPooling2D(3, 2)
 
-        self.module_1a = ResnetModule('resnet_module_1a', [64, 64])
-        self.module_1b = ResnetModule('resnet_module_1b', [64, 64])
-        self.module_1c = ResnetModule('resnet_module_1c', [64, 64])
+        self.module_1a = ResnetModule('resnet_module_1a', [64, 64, 128])
+        self.module_1b = ResnetModule('resnet_module_1b', [64, 64, 128])
+        self.module_1c = ResnetModule('resnet_module_1c', [64, 64, 128])
 
-        self.module_2a = ResnetModule('resnet_module_2a', [128, 128])
-        self.module_2b = ResnetModule('resnet_module_2b', [128, 128])
-        self.module_2c = ResnetModule('resnet_module_2c', [128, 128])
-        self.module_2d = ResnetModule('resnet_module_2d', [128, 128])
+        self.module_2a = ResnetModule('resnet_module_2a', [128, 128, 256], stride=2)
+        self.module_2b = ResnetModule('resnet_module_2b', [128, 128, 256])
+        self.module_2c = ResnetModule('resnet_module_2c', [128, 128, 256])
 
         stride = 1
         dilation_rate = 1
@@ -107,48 +113,38 @@ class ResnetBackbone(tf.keras.Model):
             stride = 2
         else:
             dilation_rate = 2
-        self.module_3a = ResnetModule('resnet_module_3a', [256, 256], stride=stride,
+        self.module_3a = ResnetModule('resnet_module_3a', [256, 256, 512], stride=stride,
                                       dilation_rate=dilation_rate)
-        self.module_3b = ResnetModule('resnet_module_3b', [256, 256],
-                                      dilation_rate=dilation_rate)
-        self.module_3c = ResnetModule('resnet_module_3c', [256, 256],
+        self.module_3b = ResnetModule('resnet_module_3b', [256, 256, 512],
                                       dilation_rate=dilation_rate)
 
         if not pretrain:
             dilation_rate = 4
-        self.module_3d = ResnetModule('resnet_module_3d', [512, 512], stride=stride,
+        self.module_3d = ResnetModule('resnet_module_3d', [256, 256, 512], stride=stride,
                                       dilation_rate=dilation_rate)
-        self.module_3e = ResnetModule('resnet_module_3e', [512, 512],
-                                      dilation_rate=2*dilation_rate)
-        self.module_3f = ResnetModule('resnet_module_3f', [512, 512],
+        self.module_3e = ResnetModule('resnet_module_3e', [256, 256, 512],
                                       dilation_rate=dilation_rate)
 
     def call(self, x, train_batch_norm=False):
         x = self.first_conv(x)
-        x = tf.keras.activations.relu(x)
-        x = self.bn1(x, training=train_batch_norm)
 
         x = self.module_1a(x, train_batch_norm=train_batch_norm)
-        x = self.module_1b(x, train_batch_norm=train_batch_norm)
 
+        x = self.padding1(x)
         x = self.maxpool1(x)
 
+        x = self.module_1b(x, train_batch_norm=train_batch_norm)
         x = self.module_1c(x, train_batch_norm=train_batch_norm)
+
         x = self.module_2a(x, train_batch_norm=train_batch_norm)
-
-        x = self.maxpool2(x)
-
         x = self.module_2b(x, train_batch_norm=train_batch_norm)
         x = self.module_2c(x, train_batch_norm=train_batch_norm)
-        x = self.module_2d(x, train_batch_norm=train_batch_norm)
 
         x = self.module_3a(x, train_batch_norm=train_batch_norm)
         x = self.module_3b(x, train_batch_norm=train_batch_norm)
-        x = self.module_3c(x, train_batch_norm=train_batch_norm)
 
         x = self.module_3d(x, train_batch_norm=train_batch_norm)
         x = self.module_3e(x, train_batch_norm=train_batch_norm)
-        x = self.module_3f(x, train_batch_norm=train_batch_norm)
         return x
 
 
@@ -156,9 +152,9 @@ class ResnetBranch(tf.keras.Model):
     def __init__(self, name):
         super().__init__(name=name)
 
-        self.module_4a = ResnetModule('resnet_module_4a', [512, 512])
-        self.module_4b = ResnetModule('resnet_module_4b', [512, 512])
-        self.module_4c = ResnetModule('resnet_module_4c', [512, 512])
+        self.module_4a = ResnetModule('resnet_module_4a', [512, 512, 1024], dilation_rate=2)
+        self.module_4b = ResnetModule('resnet_module_4b', [512, 512, 1024], dilation_rate=2)
+        self.module_4c = ResnetModule('resnet_module_4c', [512, 512, 1024])
 
     def call(self, x, train_batch_norm=False):
         x = self.module_4a(x, train_batch_norm=train_batch_norm)
