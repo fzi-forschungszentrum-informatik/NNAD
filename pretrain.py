5ebb41ecc136b49e4d8e8057b769b57f3c10eb31
Niels Ole Salscheider <salscheider@fzi.de>
2020-03-17 12:24:44 +0100

    Remove DistributionStrategy
    It does not play well with OnlineNorm...

--- pretrain.py
+++ pretrain.py
@@ -41,10 +41,6 @@ config, config_path = get_config()
 # Random seed
 random.seed()
 
-# Create a distribution strategy
-distribution_strategy = tf.distribute.MirroredStrategy()
-num_gpus = distribution_strategy.num_replicas_in_sync
-
 # Create a summary writer
 train_summary_writer = tf.summary.create_file_writer(os.path.join(config['state_dir'], 'pretrain_summaries'))
 
@@ -60,76 +56,70 @@ pretrain_base_learning_rate = 1e-3
 def learning_rate_fn():
     return pretrain_base_learning_rate * (1.0 - tf.pow(global_step / max_pretrain_steps, 0.9))
 
-with distribution_strategy.scope():
-    # Create an optimizer, the network and the loss class
-    opt = tfa.optimizers.LAMB(learning_rate_fn)
+# Create an optimizer, the network and the loss class
+opt = tfa.optimizers.LAMB(learning_rate_fn)
 
-    # Models
-    backbone = ResnetBackbone('backbone', pretrain=True)
-    pretrain_head = PretrainHead('pretrain_head', config)
-    pretrain_loss = PretrainLoss('pretrain_loss', config)
+# Models
+backbone = ResnetBackbone('backbone', pretrain=True)
+pretrain_head = PretrainHead('pretrain_head', config)
+pretrain_loss = PretrainLoss('pretrain_loss', config)
 
-    # Define a training step function
-    def train_step_fn():
-        images, ground_truth, metadata = ds.get_batched_data(config['pretrain_batch_size_per_gpu'])
-        with tf.GradientTape() as tape:
-            feature_map = backbone(images['left'], config['pretrain_batch_norm'])
-            result = pretrain_head(feature_map, config['pretrain_batch_norm'])
-            loss = pretrain_loss([result, ground_truth], tf.cast(global_step, tf.int64))
+# Define a training step function
+@tf.function
+def train_step():
+    images, ground_truth, metadata = ds.get_batched_data(config['pretrain_batch_size_per_gpu'])
+    with tf.GradientTape() as tape:
+        feature_map = backbone(images['left'], config['pretrain_batch_norm'])
+        result = pretrain_head(feature_map, config['pretrain_batch_norm'])
+        loss = pretrain_loss([result, ground_truth], tf.cast(global_step, tf.int64))
 
-            loss = loss + tf.add_n(backbone.losses + pretrain_head.losses + pretrain_loss.losses)
+        loss = loss + tf.add_n(backbone.losses + pretrain_head.losses + pretrain_loss.losses)
 
-        vs = backbone.trainable_variables + pretrain_head.trainable_variables + pretrain_loss.trainable_variables
-        gs = tape.gradient(loss, vs)
-        opt.apply_gradients(zip(gs, vs))
-        return loss
+    vs = backbone.trainable_variables + pretrain_head.trainable_variables + pretrain_loss.trainable_variables
+    gs = tape.gradient(loss, vs)
+    opt.apply_gradients(zip(gs, vs))
+    return loss
 
-    @tf.function
-    def train_step():
-        per_replica_losses = distribution_strategy.extended.call_for_each_replica(train_step_fn, args=())
-        total_loss = distribution_strategy.reduce(tf.distribute.ReduceOp.MEAN, per_replica_losses, None)
-        return total_loss
+# Load checkpoints
+checkpoint = tf.train.Checkpoint(backbone=backbone, pretrain_head=pretrain_head, pretrain_loss=pretrain_loss,
+                                    optimizer=opt, pretrain_global_step=global_step)
+checkpoint_manager = tf.train.CheckpointManager(checkpoint, os.path.join(config['state_dir'], 'checkpoints'), 25)
+checkpoint.restore(checkpoint_manager.latest_checkpoint)
 
-    # Load checkpoints
-    checkpoint = tf.train.Checkpoint(backbone=backbone, pretrain_head=pretrain_head, pretrain_loss=pretrain_loss,
-                                     optimizer=opt, pretrain_global_step=global_step)
-    checkpoint_manager = tf.train.CheckpointManager(checkpoint, os.path.join(config['state_dir'], 'checkpoints'), 25)
-    checkpoint.restore(checkpoint_manager.latest_checkpoint)
+# Training loop
+step = global_step.numpy()
+summary_step = step + 20
+while step < max_pretrain_steps:
+    # Enable trace
+    if step == summary_step:
+        tf.summary.trace_on()
 
-    # Training loop
+    # Run training step
+    with train_summary_writer.as_default():
+        start_time = time.time()
+        total_loss = train_step()
+        duration = time.time() - start_time
+
+    assert not np.isnan(total_loss.numpy()), 'Model diverged with loss = NaN'
+
+    # Write training progress to console
+    if step % 10 == 0:
+        examples_per_step = config['pretrain_batch_size_per_gpu']
+        sec_per_batch = float(duration)
+        examples_per_sec = examples_per_step / duration
+
+        print('%s: step %d, lr = %e, loss = %.6f (%.1f examples/sec: %.3f sec/batch)' %
+                (datetime.now(), step, learning_rate_fn().numpy(), total_loss.numpy(), examples_per_sec,
+                sec_per_batch))
+
+    # Save trace
+    if step == summary_step:
+        tf.summary.trace_export("Trace", step)
+
+    # Save checkpoints
+    if step > 0 and (step % 1000 == 0 or (step + 1) == config['max_steps']):
+        checkpoint_manager.save(global_step)
+
+    # Increase step counter
+    global_step.assign(global_step + 1)
     step = global_step.numpy()
-    summary_step = step + 20
-    while step < max_pretrain_steps:
-        # Enable trace
-        if step == summary_step:
-            tf.summary.trace_on()
-
-        # Run training step
-        with train_summary_writer.as_default():
-            start_time = time.time()
-            total_loss = train_step()
-            duration = time.time() - start_time
-
-        assert not np.isnan(total_loss.numpy()), 'Model diverged with loss = NaN'
-
-        # Write training progress to console
-        if step % 10 == 0:
-            examples_per_step = config['pretrain_batch_size_per_gpu'] * num_gpus
-            sec_per_batch = float(duration)
-            examples_per_sec = examples_per_step / duration
-
-            print('%s: step %d, lr = %e, loss = %.6f (%.1f examples/sec: %.3f sec/batch)' %
-                  (datetime.now(), step, learning_rate_fn().numpy(), total_loss.numpy(), examples_per_sec,
-                   sec_per_batch))
-
-        # Save trace
-        if step == summary_step:
-            tf.summary.trace_export("Trace", step)
-
-        # Save checkpoints
-        if step > 0 and (step % 1000 == 0 or (step + 1) == config['max_steps']):
-            checkpoint_manager.save(global_step)
-
-        # Increase step counter
-        global_step.assign(global_step + 1)
-        step = global_step.numpy()
--- train.py
+++ train.py
@@ -43,10 +43,6 @@ config, config_path = get_config()
 # Random seed
 random.seed()
 
-# Create a distribution strategy
-distribution_strategy = tf.distribute.MirroredStrategy()
-num_gpus = distribution_strategy.num_replicas_in_sync
-
 # Create a summary writer
 train_summary_writer = tf.summary.create_file_writer(os.path.join(config['state_dir'], 'summaries'))
 
@@ -59,103 +55,97 @@ with tf.device('/cpu:0'):
 def learning_rate_fn():
     return config['base_learning_rate'] * (1.0 - tf.pow(global_step / config['max_steps'], 0.9))
 
-with distribution_strategy.scope():
-    # Create an optimizer, the network and the loss class
-    opt = tfa.optimizers.LAMB(learning_rate_fn)
+# Create an optimizer, the network and the loss class
+opt = tfa.optimizers.LAMB(learning_rate_fn)
 
-    # Models
-    backbone = ResnetBackbone('backbone')
-    heads = Heads('heads', config)
+# Models
+backbone = ResnetBackbone('backbone')
+heads = Heads('heads', config)
 
-    if config['train_labels']:
-        label_loss = LabelLoss('label_loss', config)
-    if config['train_boundingboxes']:
-        box_loss = BoxLoss('box_loss', config)
-        embedding_loss = EmbeddingLoss('embedding_loss', config)
+if config['train_labels']:
+    label_loss = LabelLoss('label_loss', config)
+if config['train_boundingboxes']:
+    box_loss = BoxLoss('box_loss', config)
+    embedding_loss = EmbeddingLoss('embedding_loss', config)
 
-    # Define a training step function for single images
-    def single_train_step_fn():
-        images, ground_truth, metadata = ds.get_batched_data(config['batch_size_per_gpu'])
+# Define a training step function for single images
+@tf.function
+def single_train_step():
+    images, ground_truth, metadata = ds.get_batched_data(config['batch_size_per_gpu'])
 
-        with tf.GradientTape(persistent=True) as tape:
-            feature_map = backbone(images['left'], config['train_batch_norm'])
-            results = heads(feature_map, config['train_batch_norm'])
+    with tf.GradientTape(persistent=True) as tape:
+        feature_map = backbone(images['left'], config['train_batch_norm'])
+        results = heads(feature_map, config['train_batch_norm'])
 
-            losses = []
-            if config['train_labels']:
-                losses += [label_loss([results, ground_truth], tf.cast(global_step, tf.int64))]
-            if config['train_boundingboxes']:
-                losses += box_loss([results, ground_truth], tf.cast(global_step, tf.int64))
-                _, _, _, embedding = heads.box_branch(feature_map, config['train_batch_norm'])
-                losses += [embedding_loss([embedding, ground_truth], tf.cast(global_step, tf.int64))]
+        losses = []
+        if config['train_labels']:
+            losses += [label_loss([results, ground_truth], tf.cast(global_step, tf.int64))]
+        if config['train_boundingboxes']:
+            losses += box_loss([results, ground_truth], tf.cast(global_step, tf.int64))
+            _, _, _, embedding = heads.box_branch(feature_map, config['train_batch_norm'])
+            losses += [embedding_loss([embedding, ground_truth], tf.cast(global_step, tf.int64))]
 
-            ## Sum up all losses
-            summed_losses = tf.add_n(losses)
-            tf.summary.scalar('summed_losses', summed_losses, tf.cast(global_step, tf.int64))
+        ## Sum up all losses
+        summed_losses = tf.add_n(losses)
+        tf.summary.scalar('summed_losses', summed_losses, tf.cast(global_step, tf.int64))
 
-            ## Regularization terms
-            regularizer_losses = backbone.losses + heads.losses
-            vs = backbone.trainable_variables + heads.trainable_variables
-            if config['train_labels']:
-                regularizer_losses += label_loss.losses
-                vs += label_loss.trainable_variables
-            if config['train_boundingboxes']:
-                regularizer_losses += box_loss.losses + embedding_loss.losses
-                vs += box_loss.trainable_variables + embedding_loss.trainable_variables
-            total_loss = summed_losses + tf.add_n(regularizer_losses)
+        ## Regularization terms
+        regularizer_losses = backbone.losses + heads.losses
+        vs = backbone.trainable_variables + heads.trainable_variables
+        if config['train_labels']:
+            regularizer_losses += label_loss.losses
+            vs += label_loss.trainable_variables
+        if config['train_boundingboxes']:
+            regularizer_losses += box_loss.losses + embedding_loss.losses
+            vs += box_loss.trainable_variables + embedding_loss.trainable_variables
+        total_loss = summed_losses + tf.add_n(regularizer_losses)
 
-        gs = tape.gradient(total_loss, vs)
-        opt.apply_gradients(zip(gs, vs))
-        return total_loss
+    gs = tape.gradient(total_loss, vs)
+    opt.apply_gradients(zip(gs, vs))
+    return total_loss
 
-    @tf.function
-    def train_step():
-        per_replica_losses = distribution_strategy.extended.call_for_each_replica(single_train_step_fn, args=())
-        total_loss = distribution_strategy.reduce(tf.distribute.ReduceOp.MEAN, per_replica_losses, None)
-        return total_loss
+# Load checkpoints
+checkpoint = tf.train.Checkpoint(backbone=backbone, heads=heads, label_loss=label_loss, box_loss=box_loss,
+                                    embedding_loss=embedding_loss, optimizer=opt, global_step=global_step)
+checkpoint_manager = tf.train.CheckpointManager(checkpoint, os.path.join(config['state_dir'], 'checkpoints'), 25)
+checkpoint.restore(checkpoint_manager.latest_checkpoint)
 
-    # Load checkpoints
-    checkpoint = tf.train.Checkpoint(backbone=backbone, heads=heads, label_loss=label_loss, box_loss=box_loss,
-                                     embedding_loss=embedding_loss, optimizer=opt, global_step=global_step)
-    checkpoint_manager = tf.train.CheckpointManager(checkpoint, os.path.join(config['state_dir'], 'checkpoints'), 25)
-    checkpoint.restore(checkpoint_manager.latest_checkpoint)
+# Training loop
+step = global_step.numpy()
+summary_step = step + 1
+while step < config['max_steps']:
+    # Enable trace
+    if step == summary_step:
+        tf.summary.trace_on(graph=True, profiler=True)
 
-    # Training loop
-    step = global_step.numpy()
-    summary_step = step + 1
-    while step < config['max_steps']:
-        # Enable trace
-        if step == summary_step:
-            tf.summary.trace_on(graph=True, profiler=True)
+    # Run training step
+    with train_summary_writer.as_default():
+        start_time = time.time()
+        total_loss = single_train_step()
+        duration = time.time() - start_time
 
-        # Run training step
+    assert not np.isnan(total_loss.numpy()), 'Model diverged with loss = NaN'
+
+    # Write training progress to console
+    if step % 10 == 0:
+        examples_per_step = config['batch_size_per_gpu']
+        sec_per_batch = float(duration)
+        examples_per_sec = examples_per_step / duration
+
+        print('%s: step %d, lr = %e, loss = %.6f (%.1f examples/sec: %.3f sec/batch)' %
+                (datetime.now(), step, learning_rate_fn().numpy(), total_loss.numpy(), examples_per_sec,
+                sec_per_batch))
+
+    # Save trace
+    if step == summary_step:
         with train_summary_writer.as_default():
-            start_time = time.time()
-            total_loss = train_step()
-            duration = time.time() - start_time
+            tf.summary.trace_export("Trace %s" % datetime.now(), step,
+                                    profiler_outdir=os.path.join(config['state_dir'], 'summaries'))
 
-        assert not np.isnan(total_loss.numpy()), 'Model diverged with loss = NaN'
+    # Save checkpoints
+    if step > 0 and (step % 1000 == 0 or (step + 1) == config['max_steps']):
+        checkpoint_manager.save(global_step)
 
-        # Write training progress to console
-        if step % 10 == 0:
-            examples_per_step = config['batch_size_per_gpu'] * num_gpus
-            sec_per_batch = float(duration)
-            examples_per_sec = examples_per_step / duration
-
-            print('%s: step %d, lr = %e, loss = %.6f (%.1f examples/sec: %.3f sec/batch)' %
-                  (datetime.now(), step, learning_rate_fn().numpy(), total_loss.numpy(), examples_per_sec,
-                   sec_per_batch))
-
-        # Save trace
-        if step == summary_step:
-            with train_summary_writer.as_default():
-                tf.summary.trace_export("Trace %s" % datetime.now(), step,
-                                        profiler_outdir=os.path.join(config['state_dir'], 'summaries'))
-
-        # Save checkpoints
-        if step > 0 and (step % 1000 == 0 or (step + 1) == config['max_steps']):
-            checkpoint_manager.save(global_step)
-
-        # Increase step counter
-        global_step.assign(global_step + 1)
-        step = global_step.numpy()
+    # Increase step counter
+    global_step.assign(global_step + 1)
+    step = global_step.numpy()
