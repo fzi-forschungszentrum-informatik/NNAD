e07cb17002d8e24ce87db9917ac03343ddd3c071
Niels Ole Salscheider <salscheider@fzi.de>
2020-03-17 11:58:43 +0100

    Find libtensorflow_framework 2

--- data/cmake/FindTensorflow.cmake
+++ data/cmake/FindTensorflow.cmake
@@ -2,7 +2,7 @@ execute_process(COMMAND python3 -c "import sys, os; sys.stdout = open(os.devnull
 execute_process(COMMAND python3 -c "import sys, os; sys.stdout = open(os.devnull, 'w'); import tensorflow as tf; sys.stdout = sys.__stdout__; print(tf.sysconfig.get_link_flags()[0][2:])" OUTPUT_VARIABLE TF_LIB_DIR OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 find_path(TENSORFLOW_INCLUDE_DIR tensorflow/core/public/session.h HINTS ${TF_INCLUDE_DIR} NO_DEFAULT_PATH)
-find_library(TENSORFLOW_LIBRARY libtensorflow_framework.so.1 HINTS ${TF_LIB_DIR} NO_DEFAULT_PATH)
+find_library(TENSORFLOW_LIBRARY libtensorflow_framework.so.2 HINTS ${TF_LIB_DIR} NO_DEFAULT_PATH)
 
 set(TENSORFLOW_INCLUDE_DIRS ${TENSORFLOW_INCLUDE_DIR} "${TENSORFLOW_INCLUDE_DIR}/external/protobuf_archive/src/")
 set(TENSORFLOW_LIBRARIES ${TENSORFLOW_LIBRARY})
