72fdab82a64ce6bf7ea86b0ff4e494327dc40906
Niels Ole Salscheider <salscheider@fzi.de>
2020-03-24 17:36:39 +0100

    Fix cmake warnings

--- data/cmake/FindJsonCpp.cmake
+++ data/cmake/FindJsonCpp.cmake
@@ -5,6 +5,6 @@ set(JSONCPP_INCLUDE_DIRS ${JSONCPP_INCLUDE_DIR})
 set(JSONCPP_LIBRARIES ${JSONCPP_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(jsoncpp DEFAULT_MSG JSONCPP_INCLUDE_DIR JSONCPP_LIBRARY)
+find_package_handle_standard_args(JsonCpp DEFAULT_MSG JSONCPP_INCLUDE_DIR JSONCPP_LIBRARY)
 
 mark_as_advanced(JSONCPP_INCLUDE_DIR JSONCPP_LIBRARY)
